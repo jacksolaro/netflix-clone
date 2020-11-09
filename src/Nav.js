77848d02c0f2b3c12575b0e82b0f7f368ebb76d2
jacksolaro <jacksolaro@gmail.com>
2020-11-08 20:25:59 -0800

    added the nav banner

---
+++ src/Nav.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from "react";
+import "./Nav.css";
+
+function Nav() {
+  const [show, handleShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+
+  return (
+    <div className={`nav ${show && "nav_black"}`}>
+      <img
+        className="nav_logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/799px-Netflix_2015_logo.svg.png"
+        alt="netflix logo"
+      ></img>
+      <img
+        className="nav_avatar"
+        src="https://pro2-bar-s3-cdn-cf1.myportfolio.com/dddb0c1b4ab622854dd81280840458d3/98032aebff601c1d993e12a0_rw_600.png?h=8030f4d5734548795c22da59ca72e3e1"
+        alt="user icon"
+      ></img>
+    </div>
+  );
+}
+
+export default Nav;
